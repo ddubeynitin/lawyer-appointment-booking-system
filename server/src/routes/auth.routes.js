2b8ae2f77b2f09a041dd879c8cb915d5fd7cd5a2
Nitin Dubey <ddubeynitin30@gmail.com>
2026-02-26 13:34:33 +0530

    Fix Login Bug

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/auth.controller');
-const { authMiddleware } = require('../middlewares/auth.middleware');
 
 // Register route
 router.post('/register', registerUser);
 // Login route
-router.post('/login', authMiddleware, loginUser);
+router.post('/login', loginUser);
 
 module.exports = router;
