0a06e1622745c4b2212b4c0fa6652f4390250035
Nitin Dubey <ddubeynitin30@gmail.com>
2026-03-14 18:02:44 +0530

    Added two new APIs in appointment to get the appointment by user or lawyer id and updated login response data

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -126,6 +126,8 @@ const loginUser = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        phone: user.phone,
+        profileImage: user.profileImage || null,
         role: user.role,
         isProfileComplete: user.isProfileComplete,
       }
