96c3fae2fe3a471c6b6f797f773b1017bcb1c0ea
Nitin Dubey <ddubeynitin30@gmail.com>
2026-03-15 20:30:08 +0530

    Added featured lawyer route Get featured Lawyers and update featured lawyer status and updated lawyer model

--- server/src/routes/lawyer.routes.js
+++ server/src/routes/lawyer.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const upload = require("../middlewares/upload");
-const { getAllLawyers, getLawyerById, updateLawyer, completeLawyerProfile, deleteLawyer, verificationLawyer } = require('../controllers/lawyer.controller');
+const { getAllLawyers, getLawyerById, updateLawyer, completeLawyerProfile, deleteLawyer, verificationLawyer, getFeaturedLawyers, updateFeaturedLawyerStatus } = require('../controllers/lawyer.controller');
 
 // lawyer routes
 router.get('/', getAllLawyers);
 router.get('/:id', getLawyerById);
+router.get('/featured', getFeaturedLawyers);
+router.patch('/featured/:id', updateFeaturedLawyerStatus);
 router.put('/update-lawyer/:id', updateLawyer);
 router.patch('/complete-profile/:id', upload.single("profileImage"), completeLawyerProfile);
 router.delete('/delete-lawyer/:id', deleteLawyer);
