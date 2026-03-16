cc238b6885df0e9dac6eb38c1e9fa5ca124ea4bc
Nitin Dubey <ddubeynitin30@gmail.com>
2026-03-16 22:51:48 +0530

    Added Statistics API

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const appointmentRoutes = require("./src/routes/appointment.routes");
 const availabilityRoutes = require("./src/routes/availability.routes");
 const reviewRoutes = require("./src/routes/review.routes");
 const testRoutes = require("./src/routes/test.routes");
+const statRoutes = require("./src/routes/stats.routes");
 
 const app = express();
 app.use(express.json());
@@ -40,6 +41,7 @@ app.use("/api/appointments", appointmentRoutes);
 app.use("/api/availability", availabilityRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/test", testRoutes);
+app.use("/api/stats", statRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
